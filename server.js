a66b84331ff5c71c0e021d079535a297dba1d399
Joe Grotto <joegrotto@heir.company>
2017-04-23 13:18:52 -0500

    prep repo for heroku

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
+app.use(function(req, res, next) {
+    if (req.headers["x-forwarded-proto"] === "http") {
+        next();
+    } else {
+        res.redirect(`http://${req.hostname}${req.url}`);
+    }
+})
+
 app.listen(port, () => {
   console.log(`Application launched on port:${port}`);
 });
