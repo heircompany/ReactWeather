7bd4bc6d98c753afe4bee402ed73849362c79028
Joe Grotto <joegrotto@heir.company>
2017-04-23 13:28:28 -0500

    changed heroku config

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 app.use(function(req, res, next) {
-    if (req.headers["x-forwarded-proto"] === "http") {
-        next();
-    } else {
+    if (req.headers["x-forwarded-proto"] === "https") {
         res.redirect(`http://${req.hostname}${req.url}`);
+    } else {
+        next();
     }
-})
+});
 
 app.listen(port, () => {
   console.log(`Application launched on port:${port}`);
