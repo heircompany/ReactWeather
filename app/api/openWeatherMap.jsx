9492d25dd3a815f07fea4a054c7d47e4caf57d3f
Joe Grotto <joegrotto@heir.company>
2017-04-23 18:17:07 -0500

    https call

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -2,7 +2,7 @@ const
 React = require("react"),
 axios = require("axios"),
 API_KEY = "fffb28c516b89a140482b2f6698dfffa",
-OPEN_WEATHER_MAP_URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=imperial`;
+OPEN_WEATHER_MAP_URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}&units=imperial`;
 
 module.exports = {
     getTemp: function(location) {
